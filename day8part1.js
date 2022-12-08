933ea20c390ad97606ab342bfb7561603061abc5
SliverStrike123 <teh.marvin@gmail.com>
2022-12-08 22:15:04 +0800

    i forgor to press save

--- day8part1.js
+++ day8part1.js
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 
 const data = readFileSync('./input/day8.txt', 'utf8').toString().replace(/\r\n/g,"\n").split("\n");
-var visibletrees = 0
+var visibletrees = 0;
 
 for(let row in data){
     //console.log(visibletrees)
@@ -10,45 +10,45 @@ for(let row in data){
     if(row == 0){
         visibletrees += num.length
         continue
-    }
+    };
     if(row == num.length - 1){
         visibletrees += num.length
         break
-    }
+    };
     for(let i in num){
         let isvisible = false
         if(i == 0){
             visibletrees += 1
             continue
-        }
+        };
         if(i == num.length - 1){
             visibletrees += 1
             break
-        }
-        let left = []
-        let right = []
-        let up = []
-        let down = []
+        };
+        let left = [];
+        let right = [];
+        let up = [];
+        let down = [];
         for(let x = 0; x <= i;x++){
             left.push(num[x])
-        }
-        left.pop()
+        };
+        left.pop();
         for(let x = i; x < data.length;x++){
             right.push(num[x])
-        }
-        right.shift()
+        };
+        right.shift();
         for(let x = 0; x <= row;x++){
             up.push(data[x][i])
-        }
-        up.pop()
+        };
+        up.pop();
         for(let x = row; x < data.length; x++){
             down.push(data[x][i])
-        }
-        down.shift()
-        let l = 0
-        let r = 0
-        let u = 0
-        let d = 0
+        };
+        down.shift();
+        let l = 0;
+        let r = 0;
+        let u = 0;
+        let d = 0;
         left.forEach(element =>{
             
             if(parseInt(num[i]) > parseInt(element)){
@@ -91,8 +91,8 @@ for(let row in data){
             //console.log(num[i])
             visibletrees += 1
             continue
-        }
-    }
-}
+        };
+    };
+};
 
 console.log(visibletrees)
